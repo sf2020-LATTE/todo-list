d06837f45db022796a97f07795ac34867cdd27be
sf2020-LATTE <second.s.fujii@gmail.com>
2020-08-23 09:07:34 +0900

    add-destroy-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   post '/', to:'tasks#create'
-  resources :tasks, only: [:index, :create,:new]
+  resources :tasks, only: [:index, :createl,:destroy]
 end
